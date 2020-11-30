365d92d7917d16f40017f9f4055ce0d3801d589d
Fulya ÖZÇELİK <73992111+fulyabozcelik@users.noreply.github.com>
2020-11-30 21:13:02 +0300

    Arayüz tasarımı

--- main.py
+++ main.py
@@ -1,45 +1,27 @@
 import sys
 
 from PyQt5.QtCore import QUrl, QDir
-from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
-from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog, QInputDialog, QMainWindow, QLineEdit
-from form1 import *
-
-seciliListe = []
-kategoriListesi = ["Kategori Seçin", "Harfler", "Renkler", "Aylar", "Günler"]
-listem = ["araba", "berbat", "deneme", "falanca", "istanbul", "python", "şeker", "ıspanak", "bilgisayar", "telefon",
-          "araba", "acaba", "akran", "çocuk", "bilgisayar", "resim", "seçim", "ülke"]
-
+from PyQt5.QtMultimedia import QMediaContent
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
+from mainform2 import *
 
 class Kelime:
-    def __init__(self, kelime="", video=""):
+    def __init__(self,kelime,video):
         self.kelime = kelime
         self.video = video
-
     def getKelime(self):
         return self.kelime
-
     def getVideoName(self):
         return self.video
 
-
-class MyForm(QMainWindow):
+class MyForm(QDialog):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_MainWindow()
+        self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-
-        self.ui.lineEdit.textChanged.connect(self.aramaMetniDegistir)
-        self.ui.listWidget.itemClicked.connect(self.listedeKiElemanSecildi)
-        self.ui.comboBox.currentIndexChanged.connect(self.comboBoxSecim)
-        self.ui.actionKategori_Ekle.triggered.connect(self.yeniKategoriEkle)
-        self.ui.actionKategori_Sil.triggered.connect(self.kategoriSil)
-        self.ui.actionKategori_D_zenle.triggered.connect(self.kategoriDuzenle)
-        self.ui.actionRastgele_S_nav_Yap.triggered.connect(self.rastgeleSinav)
-
-        self.listeyiHazirla()
-        self.comboListeHazirla()
+        ##self.ui.pushButton.clicked.connect(self.butonTiklandi)
+        ##self.ui.lineEdit.textChanged.connect(self.aramaMetniDegistir)
+        ##self.ui.listWidget.itemClicked.connect(self.listedeKiElemanSecildi)
 
         # Video Göstericinin Kodları. layout isimli bir widgeta ekleniyor.
         self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
@@ -47,76 +29,25 @@ class MyForm(QMainWindow):
         self.ui.layout.addWidget(videoWidget)
         self.mediaPlayer.setVideoOutput(videoWidget)
         self.mediaPlayer.setMedia(
-            QMediaContent(QUrl.fromLocalFile("2.mp4")))
+                QMediaContent(QUrl.fromLocalFile("2.mp4")))
         self.mediaPlayer.play()
 
         self.show()
 
-    def kategoriDuzenle(self):
-        item, okPressed = QInputDialog.getItem(self, "Kategori Düzenleme", "Düzenlenecek Kategori:", kategoriListesi, 0, False)
-        if okPressed and item:
-            if item != "Kategori Seçin":
-                duzenlenmis, ok = QInputDialog.getText(self, "Kategori Düzenle", f"Düzenlenen Kategori:  {item}", QLineEdit.Normal, "")
-                if ok and item:
-                    # Kategoriler tablosunda düzenleme yapılacak
-                    pass
-
-
-
-    def yeniKategoriEkle(self):
-        yeniKategori, okPressed = QInputDialog.getText(self, "Kategori Ekleme", "Yeni Kategori:", QLineEdit.Normal, "")
-        if okPressed and yeniKategori != '':
-            # kategoriler tablosuna yeni kategori eklenecek
-            kategoriListesi.append(yeniKategori)
-            self.ui.comboBox.clear()
-            self.ui.comboBox.addItems(kategoriListesi)
-
-    def kategoriSil(self):
-        item, okPressed = QInputDialog.getItem(self, "Kategori Silme İşlemi", "Silineek Kategoriyi Silin:", kategoriListesi, 0, False)
-        if okPressed and item:
-            if item != "Kategori Seçin":
-                # Kategori tablosundan veri silinecek
-                kategoriListesi.remove(item)
-                self.ui.comboBox.clear()
-                self.ui.comboBox.addItems(kategoriListesi)
-
-
-
-    def comboBoxSecim(self):
-
-        kategori = self.ui.comboBox.itemText(self.ui.comboBox.currentIndex())
-        if (self.ui.comboBox.currentIndex() != 0):
-            print(kategori)
-
-    def comboListeHazirla(self):
-        self.ui.comboBox.addItems(kategoriListesi)
-
-    def listeyiHazirla(self):
-        self.ui.listWidget.addItems(listem)
-
-    def videoyuOynat(self, video):
-        self.mediaPlayer.setMedia(
-            QMediaContent(QUrl.fromLocalFile(video)))
-        self.mediaPlayer.play()
 
     def listedeKiElemanSecildi(self):
         d = self.ui.listWidget.currentItem()
+        print(d.text())
 
-    def rastgeleSinav(self):
-        pass
+
+    def butonTiklandi(self):
+        self.ui.listWidget.addItem(self.ui.lineEdit.text())
 
     def aramaMetniDegistir(self):
-        self.ui.listWidget.clear()
-        seciliListe.clear()
-        aramaMetni = self.ui.lineEdit.text()
-        for v in listem:
-            if v.startswith(aramaMetni):
-                seciliListe.append(v)
-
-        self.ui.listWidget.addItems(seciliListe)
+        print(self.ui.lineEdit.text())
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app = QApplication(sys.argv)
     w = MyForm()
     w.show()
