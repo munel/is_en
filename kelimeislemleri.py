5de5e235ab174139d1544c9ce71f77185949723b
mustafa <munel32@gmail.com>
2020-12-02 16:30:18 +0300

    Yeni Kelime Ekleme ve Silme İşlemleri. Yeni Kelime İçin Upload ve Yeniden İsimlendirme İşlemleri. Gereksiz dosyaların kaldırılması.

---
+++ kelimeislemleri.py
@@ -0,0 +1,125 @@
+import random
+
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtCore import QUrl
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtWidgets import QDialog, QWidget, QFileDialog
+
+import sqlite3
+import shutil
+import string
+conn = sqlite3.connect('Sozluk.db')
+
+
+
+class YeniKelimeEkle(QDialog):
+    def __init__(self):
+        super(YeniKelimeEkle, self).__init__()
+        self.setWindowTitle("Yeni Kelime Ekle")
+        self.setFixedSize(400, 300)
+        self.secilenler = []
+        self.kelime = ""
+        self.orjinalYol = ""
+        self.hedef = ""
+        self.uiHazirla()
+
+    def uiHazirla(self):
+        self.yeniKelimeWidget = QWidget(self)
+        self.yeniKelimeWidget.setObjectName("yeniKelimeWidget")
+        self.yeniKelimeEkleText = QtWidgets.QLineEdit(self.yeniKelimeWidget)
+        self.yeniKelimeEkleText.setGeometry(QtCore.QRect(10, 20, 131, 20))
+        self.yeniKelimeEkleText.setToolTip("")
+        self.yeniKelimeEkleText.setObjectName("yeniKelimeEkleText")
+        self.labelyk = QtWidgets.QLabel(self.yeniKelimeWidget)
+        self.labelyk.setGeometry(QtCore.QRect(10, 56, 131, 41))
+        self.labelyk.setAlignment(QtCore.Qt.AlignCenter)
+        self.labelyk.setWordWrap(True)
+        self.labelyk.setObjectName("labelyk")
+        self.listWidgetYKelime = QtWidgets.QListWidget(self.yeniKelimeWidget)
+        self.listWidgetYKelime.setGeometry(QtCore.QRect(10, 100, 131, 171))
+        self.listWidgetYKelime.setObjectName("listWidgetYKelime")
+        self.listWidgetYKelime.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.yeniKelimeWidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(159, 99, 211, 141))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.layoutYeniKelime = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.layoutYeniKelime.setContentsMargins(0, 0, 0, 0)
+        self.layoutYeniKelime.setObjectName("layoutYeniKelime")
+        self.pushButtonYKKaydet = QtWidgets.QPushButton(self.yeniKelimeWidget)
+        self.pushButtonYKKaydet.setGeometry(QtCore.QRect(210, 20, 111, 41))
+        self.pushButtonYKKaydet.setObjectName("pushButtonYKKaydet")
+        self.pushButtonYKVideoEkle = QtWidgets.QPushButton(self.yeniKelimeWidget)
+        self.pushButtonYKVideoEkle.setGeometry(QtCore.QRect(200, 250, 131, 23))
+        self.pushButtonYKVideoEkle.setObjectName("pushButtonYKVideoEkle")
+        self.yeniKelimeEkleText.setPlaceholderText("YeniKelime")
+        self.labelyk.setText("Kategori Seçin.\n"
+                                                  " Birden Fazla Seçim Yapabilirsiniz. ")
+        self.pushButtonYKKaydet.setText("Kaydet")
+        self.pushButtonYKVideoEkle.setText("Kelime Video Seç")
+
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        videoWidget = QVideoWidget()
+        self.layoutYeniKelime.addWidget(videoWidget)
+        self.mediaPlayer.setVideoOutput(videoWidget)
+
+
+        self.actionsHazirla()
+
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT GRUP_ADI FROM GRUPLAR")
+            kategoriListesiTupple = cur.fetchall()
+            self.kategoriListesi = [item[0] for item in kategoriListesiTupple]
+        self.listWidgetYKelime.addItems(self.kategoriListesi)
+
+
+
+    def actionsHazirla(self):
+        self.pushButtonYKKaydet.clicked.connect(self.yeniKelimeyiKaydetFonk)
+        self.listWidgetYKelime.itemSelectionChanged.connect(self.listedenSecimleriAyarlar)
+        self.pushButtonYKVideoEkle.clicked.connect(self.videoSec)
+
+    def videoSec(self):
+        self.orjinalYol, _ = QFileDialog.getOpenFileName(self, "Open Video")
+        if self.orjinalYol != '':
+
+            self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(self.orjinalYol)))
+            self.mediaPlayer.play()
+            dosyaAdiHam = self.orjinalYol.split("/")[-1]
+            ad = ''.join([random.choice(string.ascii_letters
+                                           ) for n in range(6)])
+            print(ad)
+            aduzanti = ad + "." + dosyaAdiHam.split(".")[-1]
+            self.hedef = "VIDEOLAR/" + aduzanti
+            shutil.copy(self.orjinalYol, self.hedef)
+
+
+
+    def yeniKelimeyiKaydetFonk(self):
+        self.kelime = self.yeniKelimeEkleText.text()
+        if self.kelime == "" or len(self.secilenler)==0 or self.orjinalYol == "":
+            print("Boş Bırakıldı")
+        else:
+            with conn:
+                cur = conn.cursor()
+                cur.execute("INSERT INTO KELIMELER (KELIME_ADI,KELIME_YOLU) VALUES(?,?)",[self.kelime.upper(),self.hedef])
+                kelimeId = cur.lastrowid
+                for g in self.secilenler:
+                    cur.execute("SELECT ID FROM GRUPLAR WHERE GRUP_ADI=(?)",[g])
+                    idH = cur.fetchone()
+                    groupId = idH[0]
+                    cur.execute("INSERT INTO GRUP_KELIMELERI (GRUP_ID,KELIME_ID) VALUES(?,?)",[groupId,kelimeId])
+                self.done(1)
+
+
+
+
+
+    def listedenSecimleriAyarlar(self):
+        sectim = self.listWidgetYKelime.selectedItems()
+        self.secilenler = [s.text() for s in sectim]
+        print(self.secilenler)
+
+
+
--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 import sys
-
 from PyQt5.QtCore import QUrl, QDir
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog, QInputDialog, QMainWindow, QLineEdit
-from form1 import *
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog, QInputDialog, QMainWindow, QLineEdit, QMessageBox
+from kelimeislemleri import YeniKelimeEkle
+from form import *
 import sqlite3
 
 conn = sqlite3.connect('Sozluk.db')
-cur = conn.cursor()
-cur.execute("SELECT KELIME_ADI FROM KELIMELER")
-kelimeListesiTupple = cur.fetchall()
-kelimeListesi = [item[0] for item in kelimeListesiTupple]
-cur.execute("SELECT GRUP_ADI FROM GRUPLAR")
-kategoriListesiTupple = cur.fetchall()
-kategoriListesi = [item[0] for item in kategoriListesiTupple]
-kategoriListesi.insert(0,"Kategori Seçin")
-
-seciliListe = []
-# kategoriListesi = ["Kategori Seçin", "Harfler", "Renkler", "Aylar", "Günler"]
-# kelimeListesi = ["araba", "berbat", "deneme", "falanca", "istanbul", "python", "şeker", "ıspanak", "bilgisayar", "telefon",
-#           "araba", "acaba", "akran", "çocuk", "bilgisayar", "resim", "seçim", "ülke"]
-
 
 class MyForm(QMainWindow):
     def __init__(self):
@@ -36,38 +22,91 @@ class MyForm(QMainWindow):
         self.ui.actionKategori_Sil.triggered.connect(self.kategoriSil)
         self.ui.actionKategori_D_zenle.triggered.connect(self.kategoriDuzenle)
         self.ui.actionRastgele_S_nav_Yap.triggered.connect(self.rastgeleSinav)
+        self.ui.actionKelime_Ekle.triggered.connect(self.yeniKelimeEkle)
+        self.ui.actionKelime_Sil.triggered.connect(self.kelimeSil)
+
+        self.kelimeListesi = []
+        self.kategoriListesi = []
+        self.seciliListe = []
+        self.listeleriHazirla()
 
         self.listeyiHazirla()
         self.comboListeHazirla()
 
-        # Video Göstericinin Kodları. layout isimli bir widgeta ekleniyor.
         self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         videoWidget = QVideoWidget()
         self.ui.layout.addWidget(videoWidget)
         self.mediaPlayer.setVideoOutput(videoWidget)
-        self.mediaPlayer.setMedia(
-            QMediaContent(QUrl.fromLocalFile("2.mp4")))
+        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile("VIDEOLAR/RAHAT.mp4")))
         self.mediaPlayer.play()
 
         self.show()
 
+    def listeleriHazirla(self):
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT KELIME_ADI FROM KELIMELER")
+            kelimeListesiTupple = cur.fetchall()
+            cur.execute("SELECT GRUP_ADI FROM GRUPLAR")
+            kategoriListesiTupple = cur.fetchall()
+        self.kelimeListesi = [item[0] for item in kelimeListesiTupple]
+        self.kategoriListesi = [item[0] for item in kategoriListesiTupple]
+        self.kategoriListesi.insert(0, "Kategori Seçin")
+
+
+    def yeniKelimeEkle(self):
+        try:
+            self.yenikelimeEkle = YeniKelimeEkle()
+            self.yenikelimeEkle.show()
+            if self.yenikelimeEkle.close:
+                print("Deneme")
+        except Exception as e:
+            print(e)
+        if self.yenikelimeEkle.exec_() == 1:
+            self.listeleriHazirla()
+            self.ui.listWidget.clear()
+            self.ui.listWidget.addItems(self.kelimeListesi)
+            QMessageBox.information(self, "Yeni Kelime", "Yeni Kelime Eklendi")
+
+    def kelimeSil(self):
+        self.listeleriHazirla()
+        item, okPressed = QInputDialog.getItem(self, "Kelime Silme İşlemi", "Silineek Kelimeyi Seçin:",
+                                               self.kelimeListesi, 0, False)
+        if okPressed and item:
+
+            try:
+                with conn:
+                    cur = conn.cursor()
+                    cur.execute("DELETE FROM KELIMELER Where KELIME_ADI=(?)", [item])
+
+
+                self.ui.comboBox.clear()
+                self.ui.listWidget.clear()
+                self.listeleriHazirla()
+                self.ui.comboBox.addItems(self.kategoriListesi)
+                self.ui.listWidget.addItems(self.kelimeListesi)
+                QMessageBox.information(self, "Kelime Silme", "Kelime Silindi")
+            except Exception as e:
+                print(e)
+
     def kategoriDuzenle(self):
-        item, okPressed = QInputDialog.getItem(self, "Kategori Düzenleme", "Düzenlenecek Kategori:", kategoriListesi, 0, False)
+        item, okPressed = QInputDialog.getItem(self, "Kategori Düzenleme", "Düzenlenecek Kategori:", self.kategoriListesi, 0,
+                                               False)
         if okPressed and item:
             if item != "Kategori Seçin":
-                duzenlenmis, ok = QInputDialog.getText(self, "Kategori Düzenle", f"Düzenlenen Kategori:  {item}", QLineEdit.Normal, "")
+                duzenlenmis, ok = QInputDialog.getText(self, "Kategori Düzenle", f"Düzenlenen Kategori:  {item}",
+                                                       QLineEdit.Normal, "")
                 if ok and item:
                     with conn:
+                        cur = conn.cursor()
                         cur.execute("UPDATE GRUPLAR SET GRUP_ADI = (?) WHERE GRUP_ADI=(?)", [duzenlenmis, item])
-                    kategoriListesi.remove(item)
-                    kategoriListesi.append(duzenlenmis)
+                    self.kategoriListesi.remove(item)
+                    self.kategoriListesi.append(duzenlenmis)
                     self.ui.comboBox.clear()
-                    self.ui.comboBox.addItems(kategoriListesi)
+                    self.ui.comboBox.addItems(self.kategoriListesi)
                     self.ui.listWidget.clear()
-                    self.ui.listWidget.addItems(kelimeListesi)
-
-
-
+                    self.ui.listWidget.addItems(self.kelimeListesi)
+                    QMessageBox.information(self, "Düzenleme", "Kategori Düzenlendi")
 
     def yeniKategoriEkle(self):
         yeniKategori, okPressed = QInputDialog.getText(self, "Kategori Ekleme", "Yeni Kategori:", QLineEdit.Normal, "")
@@ -75,42 +114,47 @@ class MyForm(QMainWindow):
             # kategoriler tablosuna yeni kategori eklenecek
             try:
                 with conn:
+                    cur = conn.cursor()
                     cur.execute("INSERT INTO GRUPLAR (GRUP_ADI) VALUES (?)",
                                 [yeniKategori])
                 print(yeniKategori)
-                kategoriListesi.append(yeniKategori)
+                self.kategoriListesi.append(yeniKategori)
                 self.ui.comboBox.clear()
-                self.ui.comboBox.addItems(kategoriListesi)
+                self.ui.comboBox.addItems(self.kategoriListesi)
                 self.ui.listWidget.clear()
-                self.ui.listWidget.addItems(kelimeListesi)
+                self.ui.listWidget.addItems(self.kelimeListesi)
+                QMessageBox.information(self, "Kategori Ekleme", "Yeni Kategori Eklendi")
             except Exception as e:
                 print(e)
 
     def kategoriSil(self):
-        item, okPressed = QInputDialog.getItem(self, "Kategori Silme İşlemi", "Silineek Kategoriyi Silin:", kategoriListesi, 0, False)
+        item, okPressed = QInputDialog.getItem(self, "Kategori Silme İşlemi", "Silineek Kategoriyi Seçin:",
+                                               self.kategoriListesi, 0, False)
         if okPressed and item:
             if item != "Kategori Seçin":
                 try:
                     with conn:
+                        cur = conn.cursor()
                         cur.execute("DELETE FROM GRUPLAR Where GRUP_ADI=(?)", [item])
 
-                    kategoriListesi.remove(item)
+                    self.kategoriListesi.remove(item)
                     self.ui.comboBox.clear()
-                    self.ui.comboBox.addItems(kategoriListesi)
+                    self.ui.comboBox.addItems(self.kategoriListesi)
                     self.ui.listWidget.clear()
-                    self.ui.listWidget.addItems(kelimeListesi)
+                    self.ui.listWidget.addItems(self.kelimeListesi)
+                    QMessageBox.information(self, "Kategroi Silme", "Kategori Silindi")
                 except Exception as e:
                     print(e)
-
-
 
     def comboBoxSecim(self):
 
         kategori = self.ui.comboBox.itemText(self.ui.comboBox.currentIndex())
         if (self.ui.comboBox.currentIndex() != 0):
             try:
-                cur.execute("SELECT KELIME_ADI FROM WR_GRUP_KELIMELERI WHERE GRUP_ADI=(?)", [kategori])
-                sonuc = cur.fetchall()
+                with conn:
+                    cur = conn.cursor()
+                    cur.execute("SELECT KELIME_ADI FROM WR_GRUP_KELIMELERI WHERE GRUP_ADI=(?)", [kategori])
+                    sonuc = cur.fetchall()
                 seciliListe = [item[0] for item in sonuc]
                 self.ui.listWidget.clear()
                 self.ui.listWidget.addItems(seciliListe)
@@ -118,10 +162,10 @@ class MyForm(QMainWindow):
                 print(e)
 
     def comboListeHazirla(self):
-        self.ui.comboBox.addItems(kategoriListesi)
+        self.ui.comboBox.addItems(self.kategoriListesi)
 
     def listeyiHazirla(self):
-        self.ui.listWidget.addItems(kelimeListesi)
+        self.ui.listWidget.addItems(self.kelimeListesi)
 
     def videoyuOynat(self, video):
         self.mediaPlayer.setMedia(
@@ -130,8 +174,10 @@ class MyForm(QMainWindow):
 
     def listedeKiElemanSecildi(self):
         d = self.ui.listWidget.currentItem()
-        cur.execute("SELECT KELIME_YOLU FROM KELIMELER Where KELIME_ADI=(?)", [d.text()])
-        sonuc = cur.fetchone()[0]
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT KELIME_YOLU FROM KELIMELER Where KELIME_ADI=(?)", [d.text()])
+            sonuc = cur.fetchone()[0]
         print(sonuc)
         self.videoyuOynat(sonuc)
 
@@ -140,13 +186,13 @@ class MyForm(QMainWindow):
 
     def aramaMetniDegistir(self):
         self.ui.listWidget.clear()
-        seciliListe.clear()
+        self.seciliListe.clear()
         aramaMetni = self.ui.lineEdit.text()
-        for v in kelimeListesi:
+        for v in self.kelimeListesi:
             if v.startswith(aramaMetni.upper()):
-                seciliListe.append(v)
+                self.seciliListe.append(v)
 
-        self.ui.listWidget.addItems(seciliListe)
+        self.ui.listWidget.addItems(self.seciliListe)
 
 
 if __name__ == "__main__":
